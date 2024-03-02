ef14a77002df298875ca6aa51ed3f987cad56346
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2024-03-02 23:20:16 +0000

    cleanup: comments

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -16,18 +16,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// Initialize a slice containing the paths to the two files. It's important
-	// to note that the file containing our base template must be the *first*
-	// file in the slice.
 	files := []string{
 		"./ui/html/base.tmpl.html",
 		"./ui/html/partials/nav.tmpl.html",
 		"./ui/html/pages/home.tmpl.html",
 	}
 
-	// Use the template.ParseFiles() function to read the files and store the
-	// templates in a template set. Notice that we can pass the slice of file
-	// paths as a variadic parameter?
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
@@ -35,8 +29,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the ExecuteTemplate() method to write the content of the "base"
-	// template as the response body.
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Println(err.Error())
