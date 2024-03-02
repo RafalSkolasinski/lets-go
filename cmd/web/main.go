1f7323e63578afb8eba0e8ddc42605b3929299f7
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2024-03-02 22:53:51 +0000

    cleanup: comments

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,16 +15,7 @@ type config struct {
 var cfg config
 
 func main() {
-	// Define a new command-line flag with the name 'addr', a default value of ":4000"
-	// and some short help text explaining what the flag controls. The value of the
-	// flag will be stored in the addr variable at runtime.
 	pflag.StringVarP(&cfg.addr, "addr", "", ":4000", "HTTP network address")
-
-	// Importantly, we use the flag.Parse() function to parse the command-line flag.
-	// This reads in the command-line flag value and assigns it to the addr
-	// variable. You need to call this *before* you use the addr variable
-	// otherwise it will always contain the default value of ":4000". If any errors are
-	// encountered during parsing the application will be terminated.
 
 	pflag.Parse()
 
@@ -37,10 +28,6 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	// The value returned from the flag.String() function is a pointer to the flag
-	// value, not the value itself. So we need to dereference the pointer (i.e.
-	// prefix it with the * symbol) before using it. Note that we're using the
-	// log.Printf() function to interpolate the address with the log message.
 	log.Printf("Starting server on %s\n", cfg.addr)
 	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
