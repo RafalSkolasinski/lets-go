9c2f2b3939a7fbc77ddb537c5ff4f563f8929445
Rafal Skolasinski <r.j.skolasinski@gmail.com>
2024-03-17 19:25:30 +0000

    db: initial connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/spf13/pflag"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+const DEFAULT_DSN = "snippetbox:snippetbox@tcp(localhost:3306)/snippetbox?parseTime=true"
 
 type application struct {
 	errorLog          *log.Logger
@@ -16,12 +21,25 @@ type application struct {
 
 func main() {
 	addr := pflag.StringP("addr", "", ":4000", "HTTP network address")
+	dsn := pflag.StringP("dsn", "", DEFAULT_DSN, "MySQL data source name")
+
 	allowFileBrowsing := pflag.BoolP("allow-file-browsing", "", false, "Disable file browsing in File Server")
 
 	pflag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// To keep the main() function tidy I've put the code for creating a connection
+	// pool into the separate openDB() function below. We pass openDB() the DSN
+	// from the command-line flag.
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	// We also defer a call to db.Close(), so that the connection pool is closed
+	// before the main() function exits.
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -39,6 +57,18 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s\n", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module letsgo.skolasinski.me
 
 go 1.22.0
 
-require github.com/spf13/pflag v1.0.5 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.8.0
+	github.com/spf13/pflag v1.0.5
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
